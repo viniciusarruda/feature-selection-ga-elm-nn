cd427df88684808f4922a827342db03ae5935479
Josias Alexandre Oliveira <josiasalexandre@gmail.com>
2019-10-12 01:48:09 -0300

    confusion matrix and another scores

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.exceptions import ConvergenceWarning
 from sklearn.utils.testing import ignore_warnings
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
+
+import matplotlib.pyplot as plt
+
 from functools import partial
 import numpy as np
 import random
@@ -40,18 +48,28 @@ def mlp_fitness(X_train, Y_train):
 
 
 @ignore_warnings(category=ConvergenceWarning)
-def mlp_train_eval(X_train, Y_train, X_test, Y_test, mask=None):
+def mlp_train_eval(X_train, Y_train, X_test, Y_test, mask=None, final_evaluation=False):
 
     if mask is not None:
         X_train = mask_input(X_train, mask)
         X_test = mask_input(X_test, mask)
 
     # should be concistent with net above
-    clf = MLPClassifier(hidden_layer_sizes=(30,), random_state=SEED, max_iter=10) # in this case, max_iter is max_epochs
-    clf.fit(X_train, Y_train) 
-    train_score = clf.score(X_train, Y_train) 
+    clf = MLPClassifier(hidden_layer_sizes=(10,), random_state=SEED, max_iter=10) # in this case, max_iter is max_epochs
+    clf.fit(X_train, Y_train)
+    train_score = clf.score(X_train, Y_train)
     test_score = clf.score(X_test, Y_test) 
-    return train_score, test_score
+
+    if final_evaluation:
+        _y_pred = clf.predict(X_test)
+        accuracy = accuracy_score(Y_test, _y_pred)
+        recall = recall_score(Y_test, _y_pred, average='weighted')
+        precision = precision_score(Y_test, _y_pred, average='weighted')
+        fmeasure = f1_score(Y_test, _y_pred, average='weighted')
+        confusion = confusion_matrix(Y_test, _y_pred)
+        return train_score, test_score, accuracy, recall, precision, fmeasure, confusion
+    else:
+        return train_score, test_score
 
 ####################################################################
 
@@ -101,7 +119,7 @@ def elm_fitness(X_train, Y_train):
     return train_score
 
 
-def elm_train_eval(X_train, Y_train, X_test, Y_test, mask=None):
+def elm_train_eval(X_train, Y_train, X_test, Y_test, mask=None, final_evaluation=False):
 
     if mask is not None:
         X_train = mask_input(X_train, mask)
@@ -110,13 +128,45 @@ def elm_train_eval(X_train, Y_train, X_test, Y_test, mask=None):
     elm = ELMRegressor(n_hidden_units=100)
     elm.fit(X_train, Y_train)
     train_score = elm.score(X_train, Y_train)
-    test_score = elm.score(X_test, Y_test) 
-    return train_score, test_score
+    test_score = elm.score(X_test, Y_test)
+
+    if final_evaluation:
+        # _y_pred = elm.predict(X_test)
+        # _y_pred = elm.predict(X_test)
+        # accuracy = accuracy_score(Y_test, _y_pred)
+        # recall = recall_score(Y_test, _y_pred)
+        # precision = precision_score(Y_test, _y_pred)
+        # fmeasure = f1_score(Y_test, _y_pred)
+        # confusion = confusion_matrix(Y_test, _y_pred)
+        return train_score, test_score, None, None, None, None, None
+    else:
+        return train_score, test_score
 
 
 ######################################################################
 
-
+# based ont the scikit learn documentation
+# https://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+def plot_confusion_matrix(cm, classes, title, normalize=False):
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    ax.figure.colorbar(im, ax=ax)
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+    
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),ha="center", va="center", color="white" if cm[i, j] > thresh else "black")
+    
+    fig.tight_layout()
+    plt.show()
 
 def mask_input(X, mask):
 
@@ -151,22 +201,29 @@ def main(net, dataset):
         net_fitness, net_train_eval = elm_fitness, elm_train_eval
     else:
         ValueError('Algorithm {} not implemented.'.format(net))
-    
 
     fitness_func = partial(fitness_func_wrapper, net_fitness, X_train, Y_train)
     
-    best, fbest = genetic_algorithm(fitness_func=fitness_func, dim=dim)
-    
-    train_score, test_score = net_train_eval(X_train, Y_train, X_test, Y_test, best)
+    best, fbest = genetic_algorithm(fitness_func=fitness_func, dim=dim, n_individuals=50)
+
+    # I have added some extra and optinal outputs in the net_train_eval function
+    # It's not elegant but...
+    train_score, test_score, accuracy, recall, precision, fmeasure, confusion = net_train_eval(X_train, Y_train, X_test, Y_test, best, final_evaluation=True)
 
     print(train_score, test_score, best.sum(), fbest)
     print('This result may match the 0.9 * (1.0 - train_score) + 0.1 * (n/nclasses) = fbest')
     print('If it does not match, it is because of the retraining of the net (net_train_eval) that used a different set of random weights.')
     print('So, it is not a bug !')
-
+    print("##############################################################################")
+    if accuracy is not None:
+        print("Let's see the accuracy:", accuracy)
+        print("Let's see the recall score:", recall)
+        print("Let's see the precision score:", precision)
+        print("Let's see the f1 score:", fmeasure)
+        plot_confusion_matrix(confusion, unique_labels(Y_test), 'Confusão')
+        print("##############################################################################")
 
 if __name__ == '__main__':
-    
     for dataset in ['breastEW', 'hepatitis', 'multiple_features']:
         for net in ['NN', 'ELM']:
             print('### Running experiment with net {} and dataset {} ###'.format(net, dataset))
